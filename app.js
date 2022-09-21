f04367cbb804fd794b7db8cc6933f6d743e9ef89
Kevin Dutier <kevin.dutier@gmail.com>
2022-09-21 11:01:58 +0200

    added moment

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require("dotenv").config({ path: ".env"});
 
+var moment = require('moment');
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
--- test.js
+++ test.js
@@ -1,11 +1,18 @@
-// import {format} from "date-fns"
+var moment = require('moment'); // require
 
-// const d= new Date()
+// const rawdate = new Date()
 
-// console.log(format(d, "dd/MM/yyyy"))
+// const formdate = moment(rawdate).format("DD/MM/YYYY")
 
-const rsa = "45"
+// console.log(formdate)
 
-const rsab = parseInt(rsa)
 
-console.log(rsab + 10)
+
+const trip = {
+    _id: "6329839b9bae9f83d2ac9a75",
+    departure: "Marseille",
+    arrival: "Bruxelles",
+    date: new Date(),
+}
+
+
