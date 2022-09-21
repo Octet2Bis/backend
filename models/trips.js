26df83ff19375d0d2d36ff3a435184565264b47c
Kevin Dutier <kevin.dutier@gmail.com>
2022-09-21 13:49:47 +0200

    added find by date

--- models/trips.js
+++ models/trips.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const tripSchema = mongoose.Schema({
     departure: String,
     arrival: String,
-    date: String,
+    date: Date,
     price: Number,
 });
  
--- test.js
+++ test.js
@@ -8,11 +8,17 @@ var moment = require('moment'); // require
 
 
 
-const trip = {
-    _id: "6329839b9bae9f83d2ac9a75",
-    departure: "Marseille",
-    arrival: "Bruxelles",
-    date: new Date(),
-}
+// const trip = {
+//     _id: "6329839b9bae9f83d2ac9a75",
+//     departure: "Marseille",
+//     arrival: "Bruxelles",
+//     date: new Date(2022, 8, 22)
+// }
+
+date = new Date(2022, 08, 22, 15, 30)
 
 
+
+
+console.log(date) 
+// output "2022-09-22T13:30:00.000Z"
